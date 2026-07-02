e46e0bf7adfadacb12bb457512adffbf05eec19c
Asil Zogby <asil.zogby@nordicsemi.no>
2026-07-02 10:18:33 +0200

    Add nrfx_secure (Draft)
    Signed-off-by: Asil Zogby <asil.zogby@nordicsemi.no>
    Co-authored-by: Sondre Pettersen <sondre.pettersen@nordicsemi.no>

--- cmake/targets/nrf9151.cmake
+++ cmake/targets/nrf9151.cmake
@@ -5,8 +5,8 @@ set(LINKER_SCRIPT_NS ${CMAKE_CURRENT_SOURCE_DIR}/scripts/linker/secure_app/nrf91
 #set(LINKER_SCRIPT ${MDK_DIR}/nrf9120_xxaa.ld)
 set(THESEUS_CPU_FLAGS -mthumb -mcpu=cortex-m33 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -g3)
 
-
-
+target_sources(${EXECUTABLE_NAME} PRIVATE ${MDK_DIR}/gcc_startup_nrf9120.S ${MDK_DIR}/system_nrf91.c)
+target_compile_definitions(${EXECUTABLE_NAME} PUBLIC NRF_TRUSTZONE_NONSECURE=1)
 #target_sources(${EXECUTABLE_NAME} PRIVATE ${MDK_DIR}/gcc_startup_nrf9160.S ${MDK_DIR}/system_nrf91.c)
 # Vendor startup file: keep it quiet without a target-wide -w
 # (which would defeat the per-source first-party warning flags applied to app.elf own sources).
@@ -16,9 +16,9 @@ set_source_files_properties(${MDK_DIR}/gcc_startup_nrf9120.S
     PROPERTIES COMPILE_OPTIONS "-w")
 
 
-target_compile_definitions(nrfx PUBLIC NRF9120_XXAA=1)
-target_sources(${EXECUTABLE_NAME} PUBLIC ${MDK_DIR}/gcc_startup_nrf9120.S ${MDK_DIR}/system_nrf91.c)
-target_compile_definitions(${EXECUTABLE_NAME} PRIVATE NRF_TRUSTZONE_NONSECURE=1)
+target_compile_definitions(nrfx_secure PUBLIC NRF9120_XXAA=1)
+target_compile_definitions(nrfx_nonsecure PUBLIC NRF9120_XXAA=1)
+
 #target_compile_definitions(nrfx PUBLIC NRF9160_XXAA=1)
 
 # Drivers excluded from the build (unused here or unsupported on nRF9151).
